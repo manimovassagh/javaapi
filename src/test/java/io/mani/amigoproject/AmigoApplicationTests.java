6541440c41e39203d58841493731aeb1fcac97f2
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-07-05 14:03:19 +0200

    1

---
+++ src/test/java/io/mani/amigoproject/AmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mani.amigoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
