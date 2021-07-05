0d16da85b608be430349e0d7db9eef708bc5975f
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-07-05 14:40:46 +0200

    under api

--- src/main/java/io/mani/amigoproject/api/PersonController.java
+++ src/main/java/io/mani/amigoproject/api/PersonController.java
@@ -1,6 +1,16 @@
 package io.mani.amigoproject.api;
 
+import io.mani.amigoproject.model.Person;
+import io.mani.amigoproject.service.PersonService;
+
 public class PersonController {
 
+private final PersonService personService;
 
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+    public void addPerson (Person person){
+        personService.addPerson(person);
+    }
 }
